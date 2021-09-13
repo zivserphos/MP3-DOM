c61e4a34124bdab8669834a492c578bce16f5e53
zivserphos <zivfromisrael@gmail.com>
2021-09-13 08:01:43 +0300

    functions i succeded
    i did not finish all the homework and i am need a little bit help after i tried for many hours

--- scripts/index.js
+++ scripts/index.js
@@ -4,28 +4,80 @@
  *
  * @param {String} songId - the ID of the song to play
  */
-function playSong(songId) {
-    // Your code here
+function playSong(songId)
+{
+    for(let song of player.songs) {
+        document.getElementById('song' + song.id).classList.remove('playing');
+        if(song.id === songId) {
+            document.getElementById('song' + song.id).classList.add('playing');
+        }
+    }
+    songIndex = sortedSongs.findIndex(x => x.id === songId);
 }
 
 /**
  * Creates a song DOM element based on a song object.
  */
-function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = []
+function createSongElement({ id, title, album, artist, duration, coverArt }) 
+{
+    var songId = document.createElement("li");
+    var songTitle = document.createElement("li");
+    var songAlbum = document.createElement("li");
+    var songArtist = document.createElement("li");
+    var songDuration = document.createElement("li");
+    var songCoverArt = document.createElement("img");
+    songCoverArt.src = coverArt
+    songId.innerText = id
+    songTitle.innerText = title
+    songAlbum.innerText = album
+    songArtist.innerText = artist
+    songDuration.innerText = duration
+    songCoverArt.innerText = coverArt
+    var div = document.createElement("div")
+    div.append(songId, songTitle, songAlbum, songArtist, songDuration, songCoverArt)
+    songLi.append(div)
+    
+    
+    
+    
+    const children = [id , title , album , artist ,duration , coverArt];
     const classes = []
-    const attrs = { onclick: `playSong(${id})` }
-    return createElement("div", children, classes, attrs)
+    const attrs = { onclick: `playSong(${id})`}
+    
+}
+
+function mmss (duration)
+{
+  let newDuration = [Math.floor(duration/60) , duration%60];
+  if (newDuration[1] < 10)
+  {
+      newDuration[1] = "0" + newDuration[1].toString();
+  }
+  if (newDuration[0] < 10)
+  {
+      newDuration[0] = "0" + newDuration[0].toString();
+  }
+  return newDuration.join(":");
 }
 
 /**
  * Creates a playlist DOM element based on a playlist object.
  */
-function createPlaylistElement({ id, name, songs }) {
-    const children = []
-    const classes = []
+function createPlaylistElement({ id, name, songs })
+ {
+    const list = []
+    var newDiv = document.createElement("div")
+    for (let key in arguments[0])
+    {  
+        let li = document.createElement("li")
+        li.innerText = arguments[0][key]
+        newDiv.appendChild(li)
+    }
+    console.log(list)
+    const children = list
+    const classes = ["A"]
     const attrs = {}
-    return createElement("div", children, classes, attrs)
+    return newDiv
 }
 
 /**
@@ -43,5 +95,19 @@ function createPlaylistElement({ id, name, songs }) {
 function createElement(tagName, children = [], classes = [], attributes = {}) {
     // Your code here
 }
+let songLi = document.getElementById("songs");
+for (let song of player.songs)
+{
+    createSongElement(song)
+}
 
+var playlist2 = document.getElementById("playlists")
+console.log(playlists)
+for (let playlist of player.playlists)
+    playlist2.append(createPlaylistElement(playlist))
+     
 // You can write more code below this line
+
+
+
+  
