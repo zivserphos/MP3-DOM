d0dedac6eaf272148461b54c26809bf3b3c82e5b
zivserphos <zivfromisrael@gmail.com>
2021-09-13 16:00:45 +0300

    improve my code
    improve my code

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,8 @@
  *
  * @param {String} songId - the ID of the song to play
  */
-function playSong(songId)
-{
-    for(let song of player.songs) {
-        document.getElementById('song' + song.id).classList.remove('playing');
-        if(song.id === songId) {
-            document.getElementById('song' + song.id).classList.add('playing');
-        }
-    }
-    songIndex = sortedSongs.findIndex(x => x.id === songId);
+function playSong(songId) {
+    // Your code here
 }
 
 /**
@@ -20,64 +13,35 @@ function playSong(songId)
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) 
 {
-    var songId = document.createElement("li");
-    var songTitle = document.createElement("li");
-    var songAlbum = document.createElement("li");
-    var songArtist = document.createElement("li");
-    var songDuration = document.createElement("li");
-    var songCoverArt = document.createElement("img");
-    songCoverArt.src = coverArt
-    songId.innerText = id
-    songTitle.innerText = title
-    songAlbum.innerText = album
-    songArtist.innerText = artist
-    songDuration.innerText = duration
-    songCoverArt.innerText = coverArt
-    var div = document.createElement("div")
-    div.append(songId, songTitle, songAlbum, songArtist, songDuration, songCoverArt)
-    songLi.append(div)
-    
-    
-    
-    
-    const children = [id , title , album , artist ,duration , coverArt];
-    const classes = []
-    const attrs = { onclick: `playSong(${id})`}
-    
-}
+    let song = arguments[0];
+    /*let songId = document.createElement("div")
+    /* artist element 
+    let songTitle = document.createElement("div")
+    let songAlbum = document.createElement("div")
+    let songArtist = document.createElement("div")
+    let songDuration = docuoment.createElement("div")*/
+    const idEl = createElement("div" , [ id])
+    const titleEl = createElement("div" , [title])
+    const albumEl = createElement("div" , [album])
+    const artistEl = createElement("div" , [artist])
+    const durationEl = createElement("div" , [duration])
+    const CoverArtEl = createElement("img" , [] ,["coverImg"] , {src: coverArt})
+    let children = ["id :" , idEl , "title: " , titleEl , "album: " , albumEl , "duration: " , duration , CoverArtEl] 
+    classes = []
 
-function mmss (duration)
-{
-  let newDuration = [Math.floor(duration/60) , duration%60];
-  if (newDuration[1] < 10)
-  {
-      newDuration[1] = "0" + newDuration[1].toString();
-  }
-  if (newDuration[0] < 10)
-  {
-      newDuration[0] = "0" + newDuration[0].toString();
-  }
-  return newDuration.join(":");
+    const attrs = { onclick: `playSong(${id})` }
+    return createElement("div", children, classes, attrs)
 }
 
 /**
  * Creates a playlist DOM element based on a playlist object.
  */
-function createPlaylistElement({ id, name, songs })
- {
-    const list = []
-    var newDiv = document.createElement("div")
-    for (let key in arguments[0])
-    {  
-        let li = document.createElement("li")
-        li.innerText = arguments[0][key]
-        newDiv.appendChild(li)
-    }
-    console.log(list)
-    const children = list
-    const classes = ["A"]
+function createPlaylistElement({ id, name, songs }) {
+    const idPl = createElement("div" , ["playlist id: " , id])
+    const children = []
+    const classes = []
     const attrs = {}
-    return newDiv
+    return createElement("div", children, classes, attrs)
 }
 
 /**
@@ -92,22 +56,31 @@ function createPlaylistElement({ id, name, songs })
  * @param {Array} classes - the class list of the new element
  * @param {Object} attributes - the attributes for the new element
  */
-function createElement(tagName, children = [], classes = [], attributes = {}) {
-    // Your code here
+function createElement(tagName, children = [], classes = [], attributes = {}) 
+{
+    let el = document.createElement(tagName);
+    
+    for (let child of children)
+    {
+        console.log(child)
+        el.append(child)
+    }
+
+    for (let cls of classes)
+    {
+        el.classList.add(cls)
+    }
+
+    for (let attr in attributes)
+    {
+        el.setAttribute(attr , attributes[attr])
+    }
+    return el
 }
-let songLi = document.getElementById("songs");
+
+// You can write more code below this line
 for (let song of player.songs)
 {
-    createSongElement(song)
+    console.log("dg")
+    document.body.append(createSongElement(song))
 }
-
-var playlist2 = document.getElementById("playlists")
-console.log(playlists)
-for (let playlist of player.playlists)
-    playlist2.append(createPlaylistElement(playlist))
-     
-// You can write more code below this line
-
-
-
-  
